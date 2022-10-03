f06fa498db3898304a344f4ed7243ec406ae70bb
Draxnos <Hajny.shane@gmail.com>
2022-10-03 14:42:43 -0500

    e

--- Assets/Scripts/DividerBehaviour.cs
+++ Assets/Scripts/DividerBehaviour.cs
@@ -12,7 +12,6 @@ public class DividerBehaviour : MonoBehaviour
 
         if (!other.gameObject.CompareTag(tag_))
         {
-            print("reversed");
             rb.velocity = -rb.velocity;
         }
     }
